72d0ac3158ed1e5e1efaa7c89ad641b997cd9c82
mixa-r <53909744+mixa-r@users.noreply.github.com>
2019-09-23 15:19:24 +0500

    Add files via upload

---
+++ bank/bank.ino
@@ -0,0 +1,44 @@
+#include <Servo.h>
+int signaldown = 10;
+int svet = 8;
+int signalup = 12;
+int inPin = 2;                   // пин для подключения датчика холла
+int inPin1 = 3;                   // пин для подключения датчика холла
+int outPin = 9;                   // пин для подключения светодиода
+Servo servo1;
+
+void setup()
+{ 
+  servo1.attach(11);   // Сервопривод сектор 1 пин 11
+  servo1.write(80);     // Сервопривод сектор 1 положение 0
+  pinMode(signalup, INPUT);
+  pinMode(signaldown, INPUT);
+  pinMode(inPin, INPUT);
+  pinMode(inPin1, INPUT);
+  pinMode(svet, INPUT);
+  pinMode(outPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  
+if( digitalRead(inPin) == LOW  && digitalRead(inPin1) == LOW ) // если кнопка тач и кнопка помада нажаты одновременно
+{
+    servo1.write(0); 
+}
+else
+{
+  servo1.write(80); 
+}
+
+
+if(digitalRead(svet) == HIGH ) // если кнопка тач и кнопка помада нажаты одновременно
+{
+    digitalWrite(outPin, HIGH);
+    }
+else
+{
+    digitalWrite(outPin, LOW);
+}}
+
---
+++ bank/proekt_2.ino
@@ -0,0 +1,40 @@
+const int LED = 9; //Pin for the LED 
+const int BUTTON = 10; //Input pin where the 
+const int Swet = 8; //pushbutton is connected 
+int endbutton = 2;
+int valbutton = 0; //Val will be used to store the state 
+int valswet = 0; //of the input pin 
+int state = 0; 
+const int LED1 = 13;
+
+
+void setup() { 
+pinMode(LED, OUTPUT); //tell aurduino LED is an output 
+pinMode(LED1, OUTPUT);
+pinMode(BUTTON, INPUT); //and BUTTON is an input 
+pinMode(endbutton, INPUT);
+pinMode(Swet, INPUT); 
+} 
+
+void loop(){ 
+valbutton = digitalRead(BUTTON); //read input value and store it 
+valswet = digitalRead(Swet); 
+
+//check wether the input is HIGH (button pressed) 
+
+if (digitalRead(endbutton) == HIGH && state==0 && valbutton==HIGH) //Если геркон на двери открыт и светит светодиод сигнализации и не перерезан провод сигнализации
+{ digitalWrite(LED1, HIGH);                                        // Включается сигнализация, необходим перезапуск игры!!!
+}
+
+if (digitalRead (endbutton) == HIGH && valbutton==HIGH)            // Если геркон на двери открыт и не перерезан провод 
+{digitalWrite(LED1, HIGH);}                                        // Включается сигнализация, необходим перезапуск игры!!!
+
+if (valswet == LOW) 
+{
+state = 1; 
+digitalWrite(LED, LOW);
+} else {
+state = 0; 
+digitalWrite(LED, HIGH); 
+}
+}
